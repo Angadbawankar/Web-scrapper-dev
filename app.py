c5adf8283a9fa761b18cd71f2345670d8eccbb7f
harshaldonarkar <97242750+harshaldonarkar@users.noreply.github.com>
2024-04-03 14:31:47 +0530

    just done some little changes.

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 from flask import Flask, render_template, request
+from pymongo import MongoClient
+import pandas as pd
 
 app = Flask(__name__)
+
+# Connect to MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+db = client['job_database']
+collection = db['job_collection']
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/page1')
+def page1():
+    # Retrieve job data from MongoDB
+    jobs_data = collection.find()
+    return render_template('page1.html', jobs_data=jobs_data)
+
 @app.route('/page2')
 def page2():
-    return render_template('page2.html')
-
-#@app.route('/page3')
-#def page3():
-#    return render_template('page3.html')
+    # Retrieve job data from MongoDB
+    jobs_data = collection.find()
+    return render_template('page2.html', jobs_data=jobs_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
